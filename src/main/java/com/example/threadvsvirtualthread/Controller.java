94fdeb36969c0ab0c59ca1ff6d36bd651e490b7a
idriss <inscrag@gmail.com>
2024-11-15 21:03:07 +0100

    first commit

---
+++ src/main/java/com/example/threadvsvirtualthread/Controller.java
@@ -0,0 +1,2 @@
+package com.example.threadvsvirtualthread;public class Controller {
+}
---
+++ src/test/java/com/example/threadvsvirtualthread/ThreadvsVirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threadvsvirtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadvsVirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
